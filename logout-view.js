a571f92304ff4cd3c6d998e6eb6d6e8382eef914
seufster <seufster@gmail.com>
2018-05-06 14:52:22 -0400

    update

---
+++ logout-view.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+export default function LogoutView({currentState, fields, texts, labels, onChangeLogin,
+  onChangeRegistration, fieldChangeEvent, onForgotPassword,
+  fieldBlurEvent, buttonClick, handleChange, changeView }) {
+
+
+    return (
+      <div>
+        <div> Logout Page </div>
+      </div>
+    );
+}
+
+
+LogoutView.propTypes = {
+  currentState: PropTypes.object,
+	fields: PropTypes.object,
+  texts: PropTypes.object,
+  labels: PropTypes.object,
+  onChangeLogin: PropTypes.func,
+  onChangeRegistration: PropTypes.func,
+  onForgotPassword: PropTypes.func,
+  fieldChangeEvent: PropTypes.func,
+  fieldBlurEvent: PropTypes.func,
+  buttonClick: PropTypes.func,
+  handleChange: PropTypes.func,
+  changeView: PropTypes.func
+};
